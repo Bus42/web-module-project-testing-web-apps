7f2cd59829f6779b5bb394d7980ffaeaf2eeec93
Greg Brewton <gbrewton1980@gmail.com>
2021-09-09 17:16:05 -0600

    Implements first 3 test cases

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import ContactForm from "./components/ContactForm";
 export default function App() {
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Integration Testing Challenge</a>
+      <nav id="navbar" className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#navbar">
+          <img
+            width="40px"
+            src="./Lambda-Logo-Red.png"
+            alt="lambda school logo"
+          />{" "}
+          Lambda Integration Testing Challenge
+        </a>
       </nav>
       <div className="App">
         <ContactForm />
